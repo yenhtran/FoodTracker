84897577ab0e902281b68de21011c56586a8a58f
Yen Tran <tran.yentran@gmail.com>
2017-11-08 13:23:58 -0800

    hook up mongoose

--- app.js
+++ app.js
@@ -1,27 +1,63 @@
 var express = require('express'),
     app = express();
     bodyPareser = require('body-parser');
+    mongoose = require('mongoose');
 
+mongoose.connect('mongodb://localhost/food_tracker');
 app.use(bodyPareser.urlencoded({extended: true}));
 app.set('view engine', 'ejs');
 
-var meals = [
-    {name: 'banana', image: 'https://source.unsplash.com/sf_1ZDA1YFw/400x300', calorie: 105},
-    {name: 'cake', image: 'https://source.unsplash.com/On2VseHUDXw/400x300', calorie: 500},
-    {name: 'salad', image: 'https://source.unsplash.com/KRGFXJWIo2Y/400x300', calorie: 150},
-    {name: 'smoothie', image: 'https://source.unsplash.com/m741tj4Cz7M/400x300', calorie: 200},
-    {name: 'cookie', image: 'https://source.unsplash.com/OfdDiqx8Cz8/400x300', calorie: 250},
-    {name: 'dumplings', image: 'https://source.unsplash.com/67mdthfNa1Y/400x300', calorie: 175},
-    {name: 'soup', image: 'https://source.unsplash.com/w6ftFbPCs9I/400x300', calorie: 150},
-    {name: 'cookie', image: 'https://source.unsplash.com/OfdDiqx8Cz8/400x300', calorie: 250},
-];
+
+var mealSchema = new mongoose.Schema({
+    name: String,
+    image: String,
+    calorie: Number
+});
+
+var Meal = mongoose.model('Meal', mealSchema);
+
+Meal.create(
+    {
+        name: 'Waffles',
+        image: 'https://source.unsplash.com/6hxK5l-sHys/400X300',
+        calorie: 150
+
+    }, function(err, meal){
+        if (err) {
+            console.log(err);
+        } else {
+            console.log('NEWLY CREATED MEAL');
+            console.log(meal);
+        }
+    });
+
+// var meals = [
+//     {name: 'banana', image: 'https://source.unsplash.com/sf_1ZDA1YFw/400x300', calorie: 105},
+//     {name: 'cake', image: 'https://source.unsplash.com/On2VseHUDXw/400x300', calorie: 500},
+//     {name: 'salad', image: 'https://source.unsplash.com/KRGFXJWIo2Y/400x300', calorie: 150},
+//     {name: 'smoothie', image: 'https://source.unsplash.com/m741tj4Cz7M/400x300', calorie: 200},
+//     {name: 'cookie', image: 'https://source.unsplash.com/OfdDiqx8Cz8/400x300', calorie: 250},
+//     {name: 'dumplings', image: 'https://source.unsplash.com/67mdthfNa1Y/400x300', calorie: 175},
+//     {name: 'soup', image: 'https://source.unsplash.com/w6ftFbPCs9I/400x300', calorie: 150},
+//     {name: 'cookie', image: 'https://source.unsplash.com/OfdDiqx8Cz8/400x300', calorie: 250},
+// ];
 
 app.get('/', function(req, res){
     res.render('landing');
 });
 
 app.get('/meals', function (req,res) {
-    res.render('meals', {meals: meals})
+
+    //get all adventures from DB
+    Meal.find({}, function(err, allmeals) {
+        if(err){
+            console.log(err);
+        } else {
+            res.render('meals', {meals: allmeals})
+        }
+    });
+
+    // res.render('meals', {meals: meals})
 });
 
 app.post('/meals', function(req, res){
@@ -30,8 +66,13 @@ app.post('/meals', function(req, res){
         calorie = req.body.calorie,
         newMeal = {name: name, image: image, calorie: calorie};
 
-    meals.push(newMeal);
-    res.redirect('/meals');
+    Meal.create(newMeal, function(err, newlyCreated){
+        if(err){
+            console.log(err);
+        } else {
+            res.redirect('/meals');
+        }
+    });
 });
 
 app.get('/meals/new', function(req, res) {
